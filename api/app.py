8aa2a74d8165561f07d405df9911caac16e63bde
Elias <esilisque@fi.uba.ar>
2025-06-16 18:25:33 -0300

    añado enpoint funcionales de login, registro, del lado del backend y frontend

--- api/app.py
+++ api/app.py
@@ -1,7 +1,8 @@
 import mysql.connector
+import requests
 from flask import Flask, request, jsonify, abort, session, redirect
 from flask_login import LoginManager
-FRONT_BASE = "http://localhost:5001"
+FRONT_BASE = "http://127.0.0.1:5001"
 app = Flask(__name__)
 app.secret_key = 'clave-super-secreta' 
 app.config['SESSION_COOKIE_DOMAIN'] = '127.0.0.1'  # Configura para localhost
@@ -49,26 +50,20 @@ def producto_detalle(producto_id):
 
 @app.route('/registro', methods=['POST'])
 def registro():
-    data = request.form.to_dict()  # Captura datos del formulario en formato diccionario
-    campos_requeridos = ['nombre', 'apellido', 'correo', 'contrasenia', 'recontrasenia']
-    if not all(campo in data for campo in campos_requeridos): #Verifica que estén los datos
-        return jsonify({"message": "Faltan datos obligatorios"}), 400
-    
-    if data['contrasenia'] != data['reconstrasenia']:
-        return jsonify({"message": "Las contraseñas no coinciden"}), 400
+    data=request.get_json()
     query_insert = """
-        INSERT INTO usuarios (nombre, apellido, correo, contrasni)
-        VALUES (%s, %s, %s)
+        INSERT INTO usuarios (nombre, apellido, mail, contrasenia)
+        VALUES (%s, %s, %s, %s)
     """
-    query_check = "SELECT * FROM usuarios WHERE correo = %s"
+    query_check = "SELECT * FROM usuarios WHERE mail = %s"
 
     try:
         coneccion = get_db()
         cursor = coneccion.cursor()
-        cursor.execute(query_check, (data['correo'],))
+        cursor.execute(query_check, (data['email'],))
         if cursor.fetchone(): #Verifica que el correo no esté registrado
             return jsonify({"message": "El correo ya está registrado"}), 400
-        values = (data['nombre'], data['apellido'], data['correo'], False)
+        values = (data['nombre'], data['apellido'], data['email'], data['contrasenia'])
         cursor.execute(query_insert, values)
         coneccion.commit()
         return redirect(f"{FRONT_BASE}/login")
@@ -89,14 +84,13 @@ def registro():
 @app.route('/usuario/login', methods=['POST'])
 def login():
     try:
-        correo = request.form['correo']
-        contrasenia = request.form['contrasenia']
-        query = "SELECT * FROM usuarios WHERE correo = %s"
+        user=request.get_json()
+        query = "SELECT * FROM usuarios WHERE mail = %s"
         connection = get_db()
-        with connection.cursor() as cursor:
-            cursor.execute(query, (correo,))
+        with connection.cursor(dictionary=True) as cursor:
+            cursor.execute(query, (user['email'],))
             usuario = cursor.fetchone()
-            if usuario and usuario['contrasenia']==contrasenia: ## Verificar si el usuario existe y la contraseña es correcta
+            if usuario and usuario['contrasenia']==user['contrasenia']: ## Verificar si el usuario existe y la contraseña es correcta
                 session['usuario'] = usuario['id']
                 session['nombre'] = usuario['nombre']
                 if usuario['administrador']:
@@ -105,6 +99,7 @@ def login():
             else:
                 return jsonify({'auth': False, "message": "Credenciales incorrectas"}), 401
     except Exception as e:
+        print(e)
         return jsonify({'auth': False, 'message': f'Error al iniciar sesión: {str(e)}'}), 500
     finally:
         connection.close()
@@ -114,7 +109,7 @@ def logout():
     session.clear()  # Limpiar todas las variables de sesión
     return redirect(f"{FRONT_BASE}/login")
 
-@app.route('/productos/categoria/<int:categoria_id>', methods=['GET'])
+@app.route('/productos/categoria/<int:categoria_id>', methods=['POST'])
 def api_categoria(categoria_id):
     db = get_db(); cur = db.cursor(dictionary=True)
     cur.execute("SELECT * FROM productos WHERE categoria=%s", (categoria_id,))
@@ -184,7 +179,7 @@ def ver_pedido(id):
 def modificar_producto():
     coneccion = get_db()
     cursor = coneccion.cursor(dictionary=True)
-    data = request.json()
+    data = request.get_json()
     id = int(data.get("categoria"))
     categoria = int(data.get("categoria"))
     nombre=data.get("nombre")
@@ -204,88 +199,6 @@ def modificar_producto():
     cursor.close()
     coneccion.close()
     return ("Producto modificado/agregado", 201)
-
-"""
-    
-"""
-
-
-@app.route('/checkout', methods=['POST'])
-def pagar():
-    data = request.get_json() or {}
-    cliente_id = data.get('cliente_id')
-    items = data.get('items')
-    if not cliente_id or not isinstance(items, list) or not items:
-        abort(400, 'cliente_id e items son requeridos')
-
-    db = get_db(); cur = db.cursor()
-    try:
-
-        # aca verificamos el  cliente
-        # ponemos la compra
-        cur.execute("INSERT INTO compras(cliente_id) VALUES(%s)", (cliente_id,))
-        compra_id = cur.lastrowid
-
-        # ponemos el detalle y verificamos el  producto
-        for it in items:
-            pid, qty = it.get('producto_id'), it.get('cantidad')
-            cur.execute("SELECT 1 FROM productos WHERE id=%s", (pid,))
-            if not cur.fetchone():
-                abort(400, f'producto_id {pid} no existe')
-            cur.execute(
-                "INSERT INTO detalle_compras(compra_id, producto_id, cantidad) "
-                "VALUES(%s,%s,%s)", (compra_id, pid, qty)
-            )
-        db.commit()
-        return jsonify({'compra_id': compra_id}), 201
-
-    except mysql.connector.Error as e:
-        db.rollback()
-        abort(400, str(e))
-
-# agrego o actualizo item del carro.
-@app.route('/api/carrito', methods=['POST'])
-def api_post_carrito():
-    data = request.get_json() or {}
-    try:
-        cid = int(data['cliente_id'])
-        pid = int(data['producto_id'])
-        qty = int(data['cantidad'])
-    except Exception:
-        abort(400, 'Se requieren cliente_id, producto_id y cantidad (enteros)')
-    if qty <= 0: 
-        abort(400, 'cantidad debe ser > 0')
-    
-    db = get_db(); cur = db.cursor()
-
-    cur.execute("SELECT 1 FROM clientes WHERE id=%s", (cid,))
-    if not cur.fetchone():
-        abort(404, 'cliente_id inválido')
-    cur.execute("SELECT stock FROM productos WHERE id=%s", (pid,))
-    row = cur.fetchone()
-    if not row:
-        abort(404, 'producto_id inválido')
-    if qty > row[0]:
-        abort(400, 'stock insuficiente')
-
-    cur.execute(
-      "SELECT id, cantidad FROM carrito WHERE cliente_id=%s AND producto_id=%s",
-      (cid, pid)
-    )
-    existing = cur.fetchone()
-    if existing:
-      new_qty = existing[1] + qty
-      cur.execute("UPDATE carrito SET cantidad=%s, fecha_agregado=NOW() WHERE id=%s",
-                  (new_qty, existing[0]))
-    else:
-      cur.execute(
-          "INSERT INTO carrito(cliente_id, producto_id, cantidad) VALUES(%s,%s,%s)",
-                  (cid, pid, qty)
-                  )
-    db.commit()
-    return jsonify({"msg":"Ítem agregado/actualizado"}), 201
-
-
 
 if __name__ == '__main__':
     app.run(port=5001)
--- app/app.py
+++ app/app.py
@@ -1,38 +1,43 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from productos_data import juegos, categorias #diccionario de ejemplo, luego cambiar por extraccion de la base datos
-
+from utils.forms import LoginForm, RegisterForm
+import requests
+import sys
+print("Python usado:", sys.executable)
 app = Flask(__name__)
 app.secret_key = 'clave-super-secreta'  # Misma clave que en el backend
 app.config['SESSION_COOKIE_DOMAIN'] = '127.0.0.1'
 app.config['SESSION_COOKIE_SECURE'] = False  # agrego el flash para cualquier mensaje para procesar "POSTs"
 
-API_BASE = "http://localhost:5000"
+API_BASE = "http://127.0.0.1:5000"
+
+categorias=["Estrategia", "Azar", "Rol", "Cartas", "Habilidad", "Cooperativos", "Solitario", "Adultos"]
 
 def obtener_productos():
-    response = request.get(f"{API_BASE}/productos")
+    response = requests.get(f"{API_BASE}/productos")
     if response.status_code == 200:
         return response.json()
     return {}
 
 def obtener_producto(id):
-    response = request.get(f"{API_BASE}/productos{id}")
+    response = requests.get(f"{API_BASE}/productos/{id}")
     if response.status_code == 200:
         return response.json()
     return {}
 
 def obtener_categoria(categoria):
-    response=request.get(f"{API_BASE}/productos/categoria/{categoria}")
+    response=requests.get(f"{API_BASE}/productos/categoria/{categoria}")
     if response.status_code==200:
         return response.json()
     return {}
 
 def obtener_inicio():
-    response=request.get(f"{API_BASE}/")
+    response=requests.get(f"{API_BASE}/")
     if response.status_code==200:
         return response.json()
     return {}
 def obtener_productos_carrito():
-    response=request.get(f"{API_BASE}/carrito")
+    response=requests.get(f"{API_BASE}/carrito")
     if response.status_code==200:
         return response.json()
     return {}
@@ -40,11 +45,11 @@ def obtener_productos_carrito():
 @app.route('/', methods=['GET'])
 def home():
     productos=obtener_inicio()
-    return render_template('home.html', categorias=[], destacados=productos['destacados'], ingresos=productos['recientes'] )
+    return render_template('home.html', categorias=categorias, destacados=productos['destacados'], ingresos=productos['recientes'] )
 
 @app.route('/productos', methods=['GET'])
 def productos():
-    return render_template('productos.html', juegos=obtener_productos(), categorias=[])
+    return render_template('productos.html', juegos=obtener_productos(), categorias=categorias)
 
 @app.route('/productos/<int:producto_id>', methods=['GET'])
 def producto_detalle(producto_id):
@@ -55,36 +60,57 @@ def producto_detalle(producto_id):
 def about_us():
     return render_template('about_us.html')
 
+
+@app.route('/FAQs', methods=['GET'])
+def faqs():
+    return render_template('faqs.html')
+
+
 @app.route('/productos/categorias/<categoria>', methods=['GET'])
 def categoria_detalle(categoria):
     return render_template('productos.html', juegos=obtener_categoria(categoria), categorias=categorias)
 
-@app.route('/login', methods=['GET'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+
+    if form.validate_on_submit(): 
+        email = form.email.data
+        password = form.password.data
+        usuario={"email":email, "contrasenia":password}
+        response = requests.post(f"{API_BASE}/usuario/login", json=usuario)
+        if response.status_code == 200:
+            return redirect(url_for('home')) 
+    return render_template('login.html', form=form)
  
 @app.route('/carrito', methods=['GET'])
 def carrito():
     return render_template('carrito.html', seleccionados=obtener_productos_carrito())
 
 
-@app.route('/contacto', methods=['GET', 'POST'])
-def contacto():
-    return render_template('contact.html')
-
-
 @app.route('/carrito/checkout')
 def checkout():
     return render_template('checkout.html')
 
-
 @app.route('/registro', methods=['GET', 'POST'])
 def registro():
-    return render_template('registro.html')
+    form = RegisterForm()
 
+    if form.validate_on_submit():
+        nombre = form.name.data.strip()
+        apellido = form.surname.data.strip()
+        email = form.email.data
+        contrasenia = form.password.data.strip()
+        usuario={"nombre": nombre, "apellido": apellido, "email":email, "contrasenia":contrasenia}
+        response = requests.post(f"{API_BASE}/registro", json=usuario)
+        if response.status_code == 200:
+            return redirect(url_for('login'))  # O tu ruta de login
+        else:
+            flash("Hubo un error al registrar. Intenta nuevamente.") 
+    return render_template('registro.html', form=form)
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(port=5001, debug=True)
 
 
 
