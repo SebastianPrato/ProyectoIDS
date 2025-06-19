7ee10ebf8c86e73245c848cb2b64451bfbe2a924
Pers <sprato@fi.uba.ar>
2025-06-18 21:29:24 -0300

    Actualización de la bbdd

--- api/crear_tablas.sql
+++ api/crear_tablas.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS productos (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id_producto INT AUTO_INCREMENT PRIMARY KEY,
+  precio INT NOT NULL;
   categoria INT NOT NULL,
   nombre VARCHAR(100) NOT NULL,
   descripcion VARCHAR(250) NOT NULL,
@@ -9,7 +10,7 @@ CREATE TABLE IF NOT EXISTS productos (
 );
 
 CREATE TABLE IF NOT EXISTS usuarios (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id_usuario INT AUTO_INCREMENT PRIMARY KEY,
   nombre VARCHAR(50) NOT NULL,
   apellido VARCHAR(50) NOT NULL,
   mail VARCHAR(100) UNIQUE NOT NULL,
@@ -18,18 +19,18 @@ CREATE TABLE IF NOT EXISTS usuarios (
 );
 
 CREATE TABLE IF NOT EXISTS compras (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id_compra INT AUTO_INCREMENT PRIMARY KEY,
   cliente_id INT NOT NULL,
   fecha DATETIME DEFAULT CURRENT_TIMESTAMP,
   entregado BOOLEAN DEFAULT FALSE,
-  FOREIGN KEY(cliente_id) REFERENCES usuarios(id)
+  pagado BOOLEAN DEFAULT FALSE,
+  FOREIGN KEY(cliente_id) REFERENCES clientes(id)
 );
 
 CREATE TABLE IF NOT EXISTS detalle_compras (
-  id INT AUTO_INCREMENT PRIMARY KEY,
   compra_id INT NOT NULL,
   producto_id INT NOT NULL,
-  cantidad INT NOT NULL,
-  FOREIGN KEY(compra_id) REFERENCES compras(id),
-  FOREIGN KEY(producto_id) REFERENCES productos(id)
+  cantidad INT NOT NULL,2
+  FOREIGN KEY(compra_id) REFERENCES compras(id_compra),
+  FOREIGN KEY(producto_id) REFERENCES productos(id_producto)
 );
