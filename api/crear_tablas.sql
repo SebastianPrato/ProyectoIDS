f94ac302ad31420d758301d18a3ab002bd91ed3b
Pers <sprato@fi.uba.ar>
2025-06-18 23:11:11 -0300

    Cambios menores

--- api/crear_tablas.sql
+++ api/crear_tablas.sql
@@ -1,6 +1,5 @@
 CREATE TABLE IF NOT EXISTS productos (
   id_producto INT AUTO_INCREMENT PRIMARY KEY,
-  precio INT NOT NULL;
   categoria INT NOT NULL,
   nombre VARCHAR(100) NOT NULL,
   descripcion VARCHAR(250) NOT NULL,
@@ -16,7 +15,7 @@ CREATE TABLE IF NOT EXISTS usuarios (
   apellido VARCHAR(50) NOT NULL,
   mail VARCHAR(100) UNIQUE NOT NULL,
   contrasenia VARCHAR(255) NOT NULL,
-  administrador BOOLEAN NOT NULL DEFAULT FALSE,
+  administrador BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 CREATE TABLE IF NOT EXISTS categoria (
@@ -30,13 +29,13 @@ CREATE TABLE IF NOT EXISTS compras (
   fecha DATETIME DEFAULT CURRENT_TIMESTAMP,
   entregado BOOLEAN DEFAULT FALSE,
   pagado BOOLEAN DEFAULT FALSE,
-  FOREIGN KEY(cliente_id) REFERENCES clientes(id)
+  FOREIGN KEY(cliente_id) REFERENCES usuarios(id_usuario)
 );
 
 CREATE TABLE IF NOT EXISTS detalle_compras (
   compra_id INT NOT NULL,
   producto_id INT NOT NULL,
-  cantidad INT NOT NULL,2
+  cantidad INT NOT NULL,
   FOREIGN KEY(compra_id) REFERENCES compras(id_compra),
   FOREIGN KEY(producto_id) REFERENCES productos(id_producto)
 );
